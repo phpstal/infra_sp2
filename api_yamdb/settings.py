53664a4fbe4b579ba5931be8f1cd0f6d1a5624dc
Vladimir <vrmy87@yandex.ru>
2021-06-11 21:36:42 +0700

    fixed some errors 3

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -4,7 +4,6 @@ from datetime import timedelta
 
 load_dotenv()
 
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
